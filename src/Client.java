0a0d6fb09e302e9611cdcf2f69bbe9a27fd0abef
Tyler Davis <tmd33196@gmail.com>
2017-03-22 11:56:43 -0500

    Server and client connected, working on sending messages

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,61 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /*
  * Client class for the server based chat project
  */
 
 public class Client {
-
+    private Socket socket = null;
+    private BufferedReader console = null;
+    private DataOutputStream output = null;
+    
+    public Client(String serverName, int serverPort){
+        System.out.println("Connecting...");
+        try{
+            socket = new Socket(serverName, serverPort);
+            System.out.println("Connected");
+            console = new BufferedReader(new InputStreamReader(System.in));
+            output = new DataOutputStream(socket.getOutputStream());
+        }catch(UnknownHostException uhe){
+            System.out.println("Host unknown: " + uhe.getMessage());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        String line = "";
+        while(!line.equals("bye")){
+            try{
+                System.out.println("Send message to server....");
+                line = console.readLine();
+                System.out.println(line);
+                output.writeUTF(line);
+                output.flush();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        try{
+            if(socket != null) socket.close();
+            if(console != null) console.close();
+            if(output != null) output.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String args[]){
+        Client client = null;
+        
+        if(args.length != 2)
+            System.out.println("Use correct input, host name, port number");
+        else
+            client = new Client(args[0], Integer.parseInt(args[1]));
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,50 @@
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /*
  * Server class for the server based chat project
  */
 
 public class Server {
-
+    private Socket socket = null;
+    private ServerSocket ss = null;
+    private DataInputStream input = null;
+    
+    public Server(int port) {
+        try {
+            ss = new ServerSocket(port);
+            System.out.println("Server started:" + ss);
+            System.out.println("Waiting for client...");
+            socket = ss.accept();
+            System.out.println("Client accepted: " + socket);
+            input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            
+            boolean done = false;
+            
+            while(!done){
+                try{
+                    String line = input.readUTF();
+                    System.out.println(line);
+                    done = line.equals("bye");
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+            if(socket!=null) socket.close();
+            if(ss!=null) ss.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String args[]){
+        Server server = null;
+        if(args.length != 1)
+            System.out.println("Use correct input of a port number");
+        else
+            server = new Server(Integer.parseInt(args[0]));
+    }
 }
